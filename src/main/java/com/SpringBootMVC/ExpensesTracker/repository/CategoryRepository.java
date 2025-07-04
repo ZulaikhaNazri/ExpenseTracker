84eb9196dd8811232b376bcd0e1a79332681054e
Shazmi Izwan <shazmiizwan@graduate.utm.my>
2025-07-04 22:06:46 +0800

    Update CategoryRepository.java

--- src/main/java/com/SpringBootMVC/ExpensesTracker/repository/CategoryRepository.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.SpringBootMVC.ExpensesTracker.repository;
 
-import com.SpringBootMVC.ExpensesTracker.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.SpringBootMVC.ExpensesTracker.entity.Category;
+
+// Repository interface for managing Category entities in the expense tracker application.
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Category findByName(String category);
 }
